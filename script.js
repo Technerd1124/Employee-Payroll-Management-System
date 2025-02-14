aee264e8ae4b693fe0aa9fb8de6d257e036c6b73
Dhanshri246 <technerd1124@gmail.com>
2025-02-14 07:11:54 +0000

    final Admin -Emplyee Login Page Done

---
+++ script.js
@@ -0,0 +1,8 @@
+
+
+ function open_AdminDashboard(){
+    window.location='Dashboard/Admin_dashboard.html';
+ }
+ function opne_EmployeeDashboard(){ 
+    window.location='Dashboard/Employee_dashboard.html'
+ }

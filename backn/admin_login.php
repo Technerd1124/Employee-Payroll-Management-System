ef3d190524d8b90694a9ad686ed58f86d7200ee3
Technerd1124 <Technerd1124@gmail.com>
2025-02-15 22:56:56 +0530

    15/02/2025

--- backn/admin_login.php
+++ backn/admin_login.php
@@ -1,6 +1,26 @@
-if($_SERVER['REQUEST_METHOD'] == "POST") { 
-    $email = POST["email"];
-    $Admin_password= POST["AdminPassword"];
-
-    
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title> Admin Dashboarc</title>
+</head>
+<body>
+  
+// Autherization of Admin from the HTML Form
+<?php
+if(isset($_POST['submit'])){
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+    if($username == "admin" && $password == "admin"){
+        header("Location: admin_dashboard.php");
+    }else{
+        echo "Invalid username or password";
+    }
 }
+
+?>
+
+<h1>Welcome admin</h1>
+</body>
+</html>

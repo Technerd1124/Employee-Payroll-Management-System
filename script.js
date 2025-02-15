ce6520afe2303459bd19c1c4842b838a7985b286
Technerd1124 <Technerd1124@gmail.com>
2025-02-15 12:18:07 +0530

    admin page render

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 
-function  OpenAdmin() {
+function OpenAdmin() {
+    document.location = "Dashboard/Admin_dashboard.html";
 
-    document.location = "Dashboard/Admin_login.html"
     
 }
 
 function OpenEmployee(){
-    document.location = "Dashboard/Employee_Dashboard.html"
+    document.location = "Dashboard/Employee_Dashboard.html";
 }

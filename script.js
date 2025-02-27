e3d5372c696ec578edf5b792f02a008dbaa1cd3c
Technerd1124 <Technerd1124@gmail.com>
2025-02-27 10:07:40 +0530

    db config

--- script.js
+++ script.js
@@ -1,61 +1,45 @@
-
 function OpenAdmin() {
-    document.location = "Dashboard/admin_login.html";
-
-    
+  document.location = "Dashboard/admin_login.html";
 }
 
-function OpenEmployee(){
-    document.location = "Dashboard/emp_login.html";
+function OpenEmployee() {
+  document.location = "Dashboard/emp_login.html";
 }
 
+function validateEmployee() {
+  var username = document.getElementById("username").value;
+  var password = document.getElementById("password").value;
 
-
-
-function validateEmployee() { 
-
-    var username = document.getElementById("username").value;
-    var password = document.getElementById("password").value;
-
-    if (username == "employee" && password == "employee") {
-        openDashboad_Emp();
-    } else {
-        alert("Invalid Username or Password");
-    }
-
+  if (username == "employee" && password == "employee") {
+    return console.log("Login Successful");
+  } else {
+    return console.log("Login Failed");
+  }
 }
-
 
 function validate_admin() {
-    // Get the email and password fields
-    var email = document.getElementsByName("email")[0].value;
-    var password = document.getElementsByName("AdminPassword")[0].value;
+  // Get the email and password fields
+  var email = document.getElementsByName("email")[0].value;
+  var password = document.getElementsByName("AdminPassword")[0].value;
 
-    // Check if email is empty
-    if (email == "") {
-        alert("Email field is required");
-        return false;
-    }
+  // Check if email is empty
+  if (email == "") &&  (password == "") {
+     return false console.log("Email field is required");
+  }
 
-    // Check if password is empty
-    if (password == "") {
-        alert("Password field is required");
-        return false;
-    }
 
-    // Check if email is in a valid format
-    var emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
-    if (!emailRegex.test(email)) {
-        alert("Invalid email format");
-        return false;
-    }
+else{
+  var emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+if (!emailRegex.test(email)) {
+  alert("Invalid email format");
+  return false;
+} 
+  if (password.length < 8) {
+    alert("Password must be at least 8 characters long");
+    return false;
+  }
 
-    // Check if password length is at least 8 characters
-    if (password.length < 8) {
-        alert("Password must be at least 8 characters long");
-        return false;
-    }
+  return alert("submitted succesfully ! ");
+}
 
-    // If all checks pass, submit the form
-    return alert("submitted succesfully ! ");
 }

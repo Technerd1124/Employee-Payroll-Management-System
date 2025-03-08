23f7c89d209545ddcc1e8a9a8cf69fb303aa6955
Technerd1124 <Technerd1124@gmail.com>
2025-03-08 21:37:55 +0530

    config db

--- DB/config.php
+++ DB/config.php
@@ -1,12 +1,13 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "ifb21";
-$dbname = "payroll";
-
-$conn = new mysqli($servername, $username, $password, $dbname);
-
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+try {
+    $servername = "localhost";
+    $username = "root";
+    $password = "ifb21";
+    $dbname = "payroll";
+    $conn = new mysqli($servername, $username, $password, $dbname);
+} catch (Exception $e) {
+    echo "Connection failed: " . $e->getMessage();
 }
+
+
 ?>

22c965eba2e7a2c257d605427962df1686af4b1f
Technerd1124 <Technerd1124@gmail.com>
2025-03-10 16:04:58 +0530

    lcoal Changes

--- DB/config.php
+++ DB/config.php
@@ -1,13 +1,15 @@
 <?php
+$servername = "localhost";
+$username = "root";
+$password = "ifb21";
+$dbname = "payroll";
+
+// Enable MySQLi error reporting (for try-catch to work)
+mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
+
 try {
-    $servername = "localhost";
-    $username = "root";
-    $password = "ifb21";
-    $dbname = "payroll";
     $conn = new mysqli($servername, $username, $password, $dbname);
+    echo "Database connected successfully!";
 } catch (Exception $e) {
     echo "Connection failed: " . $e->getMessage();
 }
-
-
-?>
